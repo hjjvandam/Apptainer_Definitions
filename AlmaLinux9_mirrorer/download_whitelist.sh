8e2a00f84063f2eaa364e26a59d6edd85a460fa0
Hubertus van Dam <hubertus.vandam@uni-due.de>
2026-06-13 00:02:54 +0200

    Script to actually download the packages and their dependencies

---
+++ AlmaLinux9_mirrorer/download_whitelist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dnf reposync --releasever=9.5 --repoid=baseos    --download-path=./mirror --download-metadata --arch=x86_64
+dnf reposync --releasever=9.5 --repoid=appstream --download-path=./mirror --download-metadata --arch=x86_64
+dnf reposync --releasever=9.5 --repoid=crb       --download-path=./mirror --download-metadata --arch=x86_64
+while read -r package || [ -n "$package" ]; do \
+    echo "Downloading: $package"; \
+    dnf download --releasever=9.5 --setopt=arch=x86_64 --setopt=ignorearch=True --repo=baseos    --resolve --alldeps --destdir=./mirror $package; \
+    dnf download --releasever=9.5 --setopt=arch=x86_64 --setopt=ignorearch=True --repo=appstream --resolve --alldeps --destdir=./mirror $package; \
+    dnf download --releasever=9.5 --setopt=arch=x86_64 --setopt=ignorearch=True --repo=crb       --resolve --alldeps --destdir=./mirror $package; \
+done < ./whitelist.txt
+#
+# To install use:
+#
+#   dnf localinstall /path/to/transferred/packages/*.rpm
